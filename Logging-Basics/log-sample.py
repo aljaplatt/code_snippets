9bee6a3b279f1b4de9c5287b78b88e645e7ed0af
Alex Platt <alja.platt@gmail.com>
2023-02-22 15:50:38 +0000

    oop

--- Logging-Basics/log-sample.py
+++ Logging-Basics/log-sample.py
@@ -1,18 +1,19 @@
 
 import logging
 
-# DEBUG: Detailed information, typically of interest only when diagnosing problems.
+#? LOGGING LEVELS - DEFAULT is warning
+#* DEBUG: Detailed information, typically of interest only when diagnosing problems.
 
-# INFO: Confirmation that things are working as expected.
+#* INFO: Confirmation that things are working as expected.
 
-# WARNING: An indication that something unexpected happened, or indicative of some problem in the near future (e.g. ‘disk space low’). The software is still working as expected.
+#! WARNING: An indication that something unexpected happened, or indicative of some problem in the near future (e.g. ‘disk space low’). The software is still working as expected. DEFAULT
 
-# ERROR: Due to a more serious problem, the software has not been able to perform some function.
+#! ERROR: Due to a more serious problem, the software has not been able to perform some function.
 
-# CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
+#! CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
 
-logging.basicConfig(filename='test.log', level=logging.DEBUG,
-                    format='%(asctime)s:%(levelname)s:%(message)s')
+# logging.basicConfig(filename='test.log', level=logging.DEBUG,
+#                     format='%(asctime)s:%(levelname)s:%(message)s')
 
 
 def add(x, y):
@@ -39,13 +40,21 @@ num_1 = 20
 num_2 = 10
 
 add_result = add(num_1, num_2)
-logging.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+# print('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+# logging.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+logging.warning('Add: {} + {} = {}'.format(num_1, num_2, add_result))
 
 sub_result = subtract(num_1, num_2)
-logging.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+# print('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+# logging.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+logging.warning('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
 
 mul_result = multiply(num_1, num_2)
-logging.debug('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
+# print('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
+# logging.debug('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
+logging.warning('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
 
 div_result = divide(num_1, num_2)
-logging.debug('Div: {} / {} = {}'.format(num_1, num_2, div_result))
+# print('Div: {} / {} = {}'.format(num_1, num_2, div_result))
+# logging.debug('Div: {} / {} = {}'.format(num_1, num_2, div_result))
+logging.warning('Div: {} / {} = {}'.format(num_1, num_2, div_result))
--- Object-Oriented/1-FirstClasses/oop.py
+++ Object-Oriented/1-FirstClasses/oop.py
@@ -1,14 +1,53 @@
 
+# class Employee:
+
+#     def __init__(self, first, last, pay):
+#         self.first = first
+#         self.last = last
+#         self.email = first + '.' + last + '@email.com'
+#         self.pay = pay
+
+#     def fullname(self):
+#         return '{} {}'.format(self.first, self.last)
+
+# emp_1 = Employee('Corey', 'Schafer', 50000)
+# emp_2 = Employee('Test', 'Employee', 60000)
+
+# class Employee:
+#     pass 
+
+# emp_1 = Employee()
+# emp_2 = Employee()
+
+# print(emp_1)
+# print(emp_2)
+
+# instance variables contain data unique to each instance 
+
 class Employee:
 
     def __init__(self, first, last, pay):
-        self.first = first
         self.last = last
         self.email = first + '.' + last + '@email.com'
         self.pay = pay
 
+        if not (first and bool(first.strip())):
+            raise ValueError("first can't be empty or null")
+        self.first = first
+
     def fullname(self):
-        return '{} {}'.format(self.first, self.last)
+        return f'{self.first} {self.last}'
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
+# emp_1 = Employee('', 'Schafer', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
+
+print(emp_1.fullname()) # Corey Schafer
+print(emp_2.fullname()) # Test Employee
+
+
+name = "alex" # True
+# name = "" # False
+# if after stripping name, name is an empty string = False, raise value error
+q = bool(name.strip())
+print("Q: ",q)
